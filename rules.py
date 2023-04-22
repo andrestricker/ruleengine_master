df51c68ed7ea67826f670cb35b638c2ea1b9ab1d
Andre Stricker <andre.stricker@acronis.com>
2023-04-22 21:36:13 +0200

    Adds system

--- rules.py
+++ rules.py
@@ -5,8 +5,11 @@ import configparser
 import uuid
 import redis
 import time
+import sysconfig
 import subprocess
 import os
+import psutil
+import platform
 import signal
 from jinja2 import Environment, FileSystemLoader
 import numpy as np
@@ -65,12 +68,15 @@ class user:
 
 class system:
     def __init__(self):
-        pass
+        self.rules = rules()
+        self.comms = redis.Redis(host=self.rules.host, port=self.rules.port,
+                                 db=self.rules.db, decode_responses=True)
 
     def selftest(self):
         res = {}
         res["redis_connection"] = self.selftest_redis()
         res["mysql_connection"] = self.selftest_mysql()
+        res["system"] = self.get_system_status()
         return res
 
     def selftest_redis(self):
@@ -94,6 +100,110 @@ class system:
                 return False
 
         return True
+
+    def get_system_status(self, logservices=False, logconnections=False):
+        systeminfo = {}
+        config = configparser.ConfigParser()
+        if os.path.isfile("agent.ini"):
+            try:
+                self.config.read('agent.ini')
+            except:
+                pass
+            else:
+                systeminfo["uuid"] = config['DEFAULT']["uuid"]
+
+        uname = platform.uname()
+        systeminfo["system"] = uname.system
+        systeminfo["os"] = platform.system()
+        systeminfo["machine"] = platform.machine()
+        systeminfo["architecture"] = platform.architecture()
+        systeminfo["platform"] = sysconfig.get_platform()
+        systeminfo["name"] = uname.node
+        systeminfo["os_release"] = uname.release
+        systeminfo["os_version"] = uname.version
+        systeminfo["machine"] = uname.machine
+        systeminfo["processor"] = uname.processor
+
+        # partitions
+        partitions = psutil.disk_partitions()
+        systeminfo["partitions"] = {}
+        for partition in partitions:
+            try:
+                partition_usage = psutil.disk_usage(partition.mountpoint)
+            except PermissionError:
+                pass
+            systeminfo["partitions"][str(partition.device)] = {
+                "mountpoint": partition.mountpoint,
+                "filesystem_type": partition.fstype,
+                "total": partition_usage.total,
+                "used": partition_usage.used,
+                "available": partition_usage.total - partition_usage.used,
+                "used_perc": partition_usage.used/(partition_usage.total/100),
+                "available_perc": 100-partition_usage.used/(partition_usage.total/100)}
+
+        ## memory ##
+        svmem = psutil.virtual_memory()
+        systeminfo["memory_total"] = svmem.total
+        systeminfo["memory_available"] = svmem.available
+        systeminfo["memory_used"] = svmem.used
+        systeminfo["memory_available_perc"] = svmem.available/(100/svmem.total)
+        systeminfo["memory_used_perc"] = svmem.used/(100/svmem.total)
+
+        swap = psutil.swap_memory()
+        systeminfo["swap_memory_total"] = swap.total
+        systeminfo["swap_memory_available"] = swap.free
+        systeminfo["swap_memory_used"] = swap.used
+        systeminfo["swap_memory_available_perc"] = swap.free/(swap.total/100)
+        systeminfo["swap_memory_used"] = swap.used/(swap.total/100)
+
+        # network connections
+        if logconnections:
+            connections = psutil.net_connections(kind='inet')
+            systeminfo["connections"] = []
+            for connection in connections:
+                systeminfo["connections"].append(connection)
+            # network interfaces
+
+        if_addrs = psutil.net_if_addrs()
+        # print(json.dumps(psutil.net_if_stats()))
+        systeminfo["network"] = []
+
+        for interface_name, interface_addresses in if_addrs.items():
+            t = {}
+            t["name"] = interface_name
+
+            for address in interface_addresses:
+                if str(address.family) == 'AddressFamily.AF_INET':
+                    t["address"] = address.address
+                    t["netmask"] = address.netmask
+                    t["broadcast"] = address.broadcast
+                   # try:
+                    #                        t["dns"] = socket.gethostbyaddr(
+                    #                           address.address)
+                   # except:
+                   #     pass
+
+            systeminfo["network"].append(t)
+        # services
+        if logservices:
+            if uname.system.upper() == "WINDOWS":
+                r = list(psutil.win_service_iter())
+                systeminfo["services"] = []
+
+                for service in r:
+                    thisservice = {}
+                    dictservice = service.as_dict()
+                    thisservice["binpath"] = dictservice["binpath"]
+                    thisservice["name"] = dictservice["name"]
+                    thisservice["start_type"] = dictservice["start_type"]
+                    thisservice["status"] = dictservice["status"]
+                    thisservice["username"] = dictservice["username"]
+                    systeminfo["services"].append(thisservice)
+
+        # configuration
+        systeminfo["services_logged"] = logservices
+        systeminfo["connections_logged"] = logconnections
+        return systeminfo
 
 
 class master:
